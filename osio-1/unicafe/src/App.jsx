f80c0a8f96a169c3f84974609a8e0578b588ee91
CalvinNFT <calvin@daysave.com>
2022-12-01 15:44:34 +0200

    1.10 tehtävä

--- osio-1/unicafe/src/App.jsx
+++ osio-1/unicafe/src/App.jsx
@@ -8,23 +8,16 @@ const Buttons = ({ handleClick, text }) => {
 	);
 };
 
-const Statistics = ({ bad, good, neutral }) => {
-	const total = bad + good + neutral;
-	const average = (good - bad) / total;
-	const positiveRatio = good / total;
-
-	if (total === 0) {
+const Statistics = (props) => {
+	if (props.total === 0) {
 		return <p>No feedback given</p>;
 	}
 
 	return (
 		<div>
-			<p>Good reviews: {good}</p>
-			<p>Neutral reviews: {neutral}</p>
-			<p>Bad reviews: {bad}</p>
-			<p>Total sum: {total} reviews</p>
-			<p>Average review: {average}</p>
-			<p>Positive of all {positiveRatio} % </p>
+			<p>
+				{props.stat}: {props.value}
+			</p>
 		</div>
 	);
 };
@@ -33,6 +26,11 @@ const App = () => {
 	const [good, setGood] = useState(0);
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
+	const total = bad + good + neutral;
+	const average = (good - bad) / total;
+	const positiveRatio = good / total;
+
+	// Stats in raw ( ok? ), advanced stats: sum, avg, posratio
 
 	return (
 		<>
@@ -43,7 +41,12 @@ const App = () => {
 				<Buttons handleClick={() => setBad(bad + 1)} text={"Bad"} />
 			</div>
 			<h1>Visitor reviews</h1>
-			<Statistics bad={bad} good={good} neutral={neutral} />
+			<Statistics stat={"good"} value={bad} />
+			<Statistics stat={"bad"} value={good} />
+			<Statistics stat={"neutral"} value={neutral} />
+			<Statistics stat={"total"} value={total} />
+			<Statistics stat={"average"} value={average} />
+			<Statistics stat={"positiveRatio"} value={positiveRatio} />
 		</>
 	);
 };
