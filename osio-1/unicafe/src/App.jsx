b583a061391ea4b0f3caf2b12e23c95b186d5578
CalvinNFT <calvin@daysave.com>
2022-12-02 09:09:13 +0200

    1.12 anecdote machine working

--- osio-1/unicafe/src/App.jsx
+++ osio-1/unicafe/src/App.jsx
@@ -53,6 +53,25 @@ const App = () => {
 	const average = (good - bad) / total;
 	const positiveRatio = good / total;
 	const content = [bad, good, neutral, total, average, positiveRatio];
+	const anecdotes = [
+		"If it hurts, do it more often.",
+		"Adding manpower to a late software project makes it later!",
+		"The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+		"Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+		"Premature optimization is the root of all evil.",
+		"Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+		"Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.",
+		'Brooks Law: "Adding manpower to a late software project makes it later!"',
+		"The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+		"Even the best planning is not so omniscient as to get it right the first time. -- Fred Brooks",
+		"Plan to throw one (implementation) away; you will, anyhow. -- Fred Brooks",
+		"Perfection (in design) is achieved not when there is nothing more to add, but rather when there is nothing more to take away --Antoine de Saint-Exupery",
+		"Program testing can be used to show the presence of bugs, but never to show their absence! --Edsger Dijkstra",
+		"Any fool can write code that a computer can understand. Good programmers write code that humans can understand. --Martin Fowler",
+		"Programming can be fun, so can cryptography; however they should not be combined. --Kreitzberg and Shneiderman ",
+	];
+
+	const [selected, setSelected] = useState(0);
 
 	// console.log(content, "App component"); // DELETE AFTER DEV
 
@@ -69,7 +88,16 @@ const App = () => {
 					<Buttons handleClick={() => setBad(bad + 1)} text={"Bad"} />
 				</div>
 				<h1>Visitor reviews</h1>
-				<p>Client feedback will be visible here!</p>;
+				<p>
+					Client feedback will be visible here! At the mean time, thy anecdotes
+				</p>
+				<div>
+					<p>{anecdotes[selected]}</p>
+					<button onClick={() => setSelected(Math.floor(Math.random() * 15))}>
+						{" "}
+						Let's hear it!
+					</button>
+				</div>
 			</>
 		);
 	}
