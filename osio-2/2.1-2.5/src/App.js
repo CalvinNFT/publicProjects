09ae7d81e3ee191d29c217f569c79132e355b2af
CalvinNFT <calvin@daysave.com>
2022-12-16 12:32:50 +0200

    2.3 functioning

--- osio-2/2.1-2.5/src/App.js
+++ osio-2/2.1-2.5/src/App.js
@@ -32,6 +32,13 @@ const App = () => {
 
 const Course = ({ course }) => {
 	const parts = course.parts;
+	const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+
+	console.log(
+		parts.reduce(function (sum, part) {
+			return sum + part.exercises;
+		}, 0)
+	);
 	return (
 		<>
 			<h1>Current course: {course.name}</h1>
@@ -42,6 +49,7 @@ const Course = ({ course }) => {
 					</li>
 				))}
 			</ul>
+			<p>Total excersises: {total} </p>
 		</>
 	);
 };
