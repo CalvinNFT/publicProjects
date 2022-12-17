11a5d1967f3604d10b5d3c1034f890e11109c19d
CalvinNFT <calvin@daysave.com>
2022-12-17 16:03:56 +0200

    WIP toimiva versio muistiinpanoista

--- osio-2/2.6-2.10/src/App.jsx
+++ osio-2/2.6-2.10/src/App.jsx
@@ -1,34 +1,51 @@
 import { useState } from "react";
 
+const Contact = ({ person }) => <li>{person.name}</li>;
+
 const App = () => {
-	const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-	const [newName, setNewName] = useState("");
+	const [persons, setPersons] = useState([{ name: "Calvin Klein" }]);
+	const [newName, setNewName] = useState("boogie man");
+	const [existingPersons, setExistingPersons] = useState([]);
 
-	// Lisätään inputtiin value="value" ja onChange=(() => setNewName({value}))
+	const addContact = (event) => {
+		event.preventDefault();
+		const contactObject = {
+			name: newName,
+		};
+		setPersons(persons.concat(contactObject));
+		// alert(`${newName} was added to contacts`); // TODO: DELETE BEFORE PUBLISHING, disabled for development
+		setNewName("");
+	};
 
-	// Set new name concatilla persons listaan ja lopuks tyhjennetään newname
-
-	// Ehdollinen muuttuja jos nimi on const setNewName = () => persons.filter({value}) > 0.5) ? persons.concat({value}) : console.alert("`${persons.name} already excists`")
-
-	// onSubmit kutsuu funktioa joka hyödyntäen setNewNamea kutsuu ehdollista muuttujaa
-
-	console.log(setNewName);
+	const handleChange = (event) => {
+		event.preventDefault;
+		setNewName(event.target.value);
+		persons.filter((existingCopies) => {
+			newName === persons.name;
+			setExistingPersons(existingCopies);
+		}); // yritetään luoda lista, jossa
+		const reject =
+			0.5 < existingPersons.lenght
+				? alert(`${NewName} is already in your contacts`)
+				: setNewName(newName);
+		reject;
+	};
 
 	return (
 		<div>
 			<h2>Phonebook</h2>
-			<form>
+			<ul>
+				{persons.map((person, i) => (
+					// added indexing to enable contacts with same names
+					<Contact person={person} key={person.name + i} />
+				))}
+			</ul>
+			<form onSubmit={addContact}>
 				<div>
-					name: <input />
+					name: <input value={newName} onChange={handleChange} />
 				</div>
 				<div>
-					<button
-						type="submit"
-						value="add contact"
-						onChange={() => setNewName(value)}
-					>
-						add
-					</button>
+					<button type="submit">add</button>
 				</div>
 			</form>
 			<h2>Numbers</h2>
