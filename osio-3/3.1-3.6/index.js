674b7fe95e72ad277f8aebf1556697fca39727c7
Calvin Otewa <calvin@daysave.com>
2022-12-29 16:41:41 +0200

    3.2

--- osio-3/3.1-3.6/index.js
+++ osio-3/3.1-3.6/index.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.use(express.json())
 
-let notes = [
+let persons = [
     {
       id: 1,
       name: "Arto Hellas",
@@ -23,13 +23,21 @@ let notes = [
     }
   ]
 
+  const date = new Date
+
+  let personsTotal = persons.length
+  console.log('length of array', personsTotal)
+
+app.get('/info', (req, res) => {
+    res.send(`<h1>Contactbook has ${personsTotal} people</h1> <br /><br /> <p>${date}</p>`)
+})
 app.get('/', (req, res) => {
-    res.send('<h1>Contactbook can be accessed from /api/persons</h1>')
+    res.send(`<h1>Looking for someone? Check info</h1>`)
 })
 
 // app.get('/api/persons/:id', (request, response) => {
 //     const id = Number(request.params.id)
-//     const note = notes.find(note => note.id === id)
+//     const note = persons.find(note => note.id === id)
 
 //     if (note) {
 //         response.json(note)
@@ -39,24 +47,24 @@ app.get('/', (req, res) => {
 //   })
 
 app.get('/api/persons', (req, res) => {
-  res.json(notes)
+  res.json(persons)
 })
 
 // app.delete('/api/persons/:id', (request, response) => {
 //     const id = Number(request.params.id)
-//     notes = notes.filter(note => note.id !== id)
+//     persons = persons.filter(note => note.id !== id)
   
 //     response.status(204).end()
 //   })
 
 // const generateId = () => {
-//     const maxId = notes.length > 0
-//       ? Math.max(...notes.map(n => n.id))
+//     const maxId = persons.length > 0
+//       ? Math.max(...persons.map(n => n.id))
 //       : 0
 //     return maxId + 1
 // }
   
-// app.post('/api/notes', (request, response) => {
+// app.post('/api/persons', (request, response) => {
 //     const body = request.body
   
 //     if (!body.content) {
@@ -72,7 +80,7 @@ app.get('/api/persons', (req, res) => {
 //       id: generateId(),
 //     }
   
-//     notes = notes.concat(note)
+//     persons = persons.concat(note)
   
 //     response.json(note)
 // })
