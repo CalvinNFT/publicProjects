c30c8516c6f453258d3a8fc2cd8b1315bac3da78
Calvin Otewa <calvin@daysave.com>
2022-12-29 17:21:24 +0200

    3.4

--- osio-3/3.1-3.6/index.js
+++ osio-3/3.1-3.6/index.js
@@ -23,17 +23,19 @@ let persons = [
     }
   ]
 
-  const date = new Date
+const date = new Date
+let personsTotal = persons.length
+console.log('length of array', personsTotal)
 
-  let personsTotal = persons.length
-  console.log('length of array', personsTotal)
 
 app.get('/info', (req, res) => {
     res.send(`<h1>Contactbook has ${personsTotal} people</h1> <br /><br /> <p>${date}</p>`)
 })
+
 app.get('/', (req, res) => {
     res.send(`<h1>Looking for someone? Check info</h1>`)
 })
+
 app.get('/api/persons/:id', (req, res) => {
   let id = Number(req.params.id)
   let person = persons.find(person => person.id === id)
@@ -45,55 +47,17 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
-// app.get('/api/persons/:id', (request, response) => {
-//     const id = Number(request.params.id)
-//     const note = persons.find(note => note.id === id)
-
-//     if (note) {
-//         response.json(note)
-//       } else {
-//         response.status(404).end()
-//       }
-//   })
-
 app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
-// app.delete('/api/persons/:id', (request, response) => {
-//     const id = Number(request.params.id)
-//     persons = persons.filter(note => note.id !== id)
-  
-//     response.status(204).end()
-//   })
+app.delete('/api/persons/:id', (req,res) => {
+  const id = Number(req.params.id)
 
-// const generateId = () => {
-//     const maxId = persons.length > 0
-//       ? Math.max(...persons.map(n => n.id))
-//       : 0
-//     return maxId + 1
-// }
-  
-// app.post('/api/persons', (request, response) => {
-//     const body = request.body
-  
-//     if (!body.content) {
-//       return response.status(400).json({ 
-//         error: 'content missing' 
-//       })
-//     }
-  
-//     const note = {
-//       content: body.content,
-//       important: body.important || false,
-//       date: new Date(),
-//       id: generateId(),
-//     }
-  
-//     persons = persons.concat(note)
-  
-//     response.json(note)
-// })
+  console.log('person deleted');
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
